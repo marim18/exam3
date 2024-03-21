70a9652917ab190387c67583e5feaeeb2b38b82b
Maria Møll <marim18@uia.no>
2024-03-21 19:44:07 +0100

    start

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+double SquarifyNumber(double Number)
+{
+
+    return Number * Number;
+}
+
+double ConvertInchToMm(double Number)
+{
+    double ConvertionConstant = 25.4;
+    return Number * ConvertionConstant;
+}
+
+int RootifyNumber(int Number)
+{
+    for (int i = 0; i < Number; i++) 
+    { 
+        if(i*i == Number){
+            return i;
+        }
+      
+    }
+     Console.WriteLine("Error this function only supports whole numbers something wrong happened");
+    return 0;
+}
+double CubeifyNumber(double Number)
+{
+
+    return Number * Number * Number;
+}
+double CalculateRadiusOfACircle(){
+    double answer = 0;
+    return answer;
+}
