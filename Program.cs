2cb0fcb03b84a9dccaa1190e582dce75a61eb984
Maria Møll <marim18@uia.no>
2024-03-21 20:14:08 +0100

    remaining of task 1 and task 2

--- Program.cs
+++ Program.cs
@@ -7,22 +7,23 @@ double SquarifyNumber(double Number)
     return Number * Number;
 }
 
-double ConvertInchToMm(double Number)
+double ConvertInchToMm(double Inches)
 {
     double ConvertionConstant = 25.4;
-    return Number * ConvertionConstant;
+    return Inches * ConvertionConstant;
 }
 
 int RootifyNumber(int Number)
 {
-    for (int i = 0; i < Number; i++) 
-    { 
-        if(i*i == Number){
+    for (int i = 0; i < Number; i++)
+    {
+        if (i * i == Number)
+        {
             return i;
         }
-      
+
     }
-     Console.WriteLine("Error this function only supports whole numbers something wrong happened");
+    Console.WriteLine("Error this function only supports whole numbers something wrong happened");
     return 0;
 }
 double CubeifyNumber(double Number)
@@ -30,7 +31,24 @@ double CubeifyNumber(double Number)
 
     return Number * Number * Number;
 }
-double CalculateRadiusOfACircle(){
-    double answer = 0;
-    return answer;
+double CalculateRadiusOfACircle(double Radius)
+{
+    double pi = 3.14159;
+    double NoMagicTwo = 2;
+    return NoMagicTwo * pi * Radius;
+}
+string GreetingsSir(string Name)
+{
+    string GreetingName = $"Hello there {Name} Fancy seeing you here";
+    return GreetingName;
+}
+
+string ArrayFlattener(int[] objects)
+{ string FlattArray = "";
+    int Lenght = objects.Length;
+    for (int i = 0; i < Lenght; i++)
+    {
+        FlattArray += objects[i];
+    }
+    return FlattArray;
 }
