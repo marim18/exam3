ef0692bfb22995fb6aa065ff797c69c0d721d8d7
Maria Møll <marim18@uia.no>
2024-03-23 19:38:21 +0100

    task 4 wip name sort completed chronologicallist

--- task4.cs
+++ task4.cs
@@ -3,34 +3,34 @@ namespace Exam3
 {
     class task4
     {
-        public List<books> BookList = new List<books>();
+        public List<Books> BookList = new List<Books>();
 
         public void reader() //redundant refractor at the end ot one function for all if oyu have time
         {
             string BookString = File.ReadAllText("books.json");
-            books book = JsonSerializer.Deserialize<books>(BookString);
+            Books book = JsonSerializer.Deserialize<Books>(BookString);
             BookList.Add(book);
         }
-        List<books> ReturnBookByKeyword(string keyword = "the")
-        {  List<books> BooksWithKeyword = new List<books>();
-            foreach(books book in BookList){
+        List<Books> ReturnBookByKeyword(string keyword = "the")
+        {  List<Books> BooksWithKeyword = new List<Books>();
+            foreach(Books book in BookList){
                 if( book.title.Contains(keyword)){
                 BooksWithKeyword.Add(book);} 
             }
             return BooksWithKeyword;
         }
 
-        List<books> ReturnBookByAuthorContainsLetter(char Keyletter = 't')
-        {    List<books> BooksWithKeyword = new List<books>();
-            foreach(books book in BookList){
+        List<Books> ReturnBookByAuthorContainsLetter(char Keyletter = 't')
+        {    List<Books> BooksWithKeyword = new List<Books>();
+            foreach(Books book in BookList){
                 if( book.author.ToLower().Contains(Keyletter)){
                 BooksWithKeyword.Add(book);} 
             }
              return BooksWithKeyword;
         }
         int NumberOfBooksAfterYear(int Year = 1992)
-        {List<books> BooksWithKeyword = new List<books>();
-            foreach(books book in BookList){
+        {List<Books> BooksWithKeyword = new List<Books>();
+            foreach(Books book in BookList){
                 if( book.publication_year > Year){
                 BooksWithKeyword.Add(book);} 
             }
@@ -38,8 +38,8 @@ namespace Exam3
 
         }
         int NumberOfBooksBeforeYear(int Year = 2004)
-        {List<books> BooksWithKeyword = new List<books>();
-            foreach(books book in BookList){
+        {List<Books> BooksWithKeyword = new List<Books>();
+            foreach(Books book in BookList){
                 if( book.publication_year < Year){
                 BooksWithKeyword.Add(book);} 
             }
@@ -48,22 +48,63 @@ namespace Exam3
         }
         List<string> ISBNAuthorReturn(string authorName)
         {   List<string> ISBNS = new List<string>();
-            foreach(books book in BookList){
+            foreach(Books book in BookList){
                 if( book.author == authorName){
                 ISBNS.Add(book.isbn);} 
             }
             return ISBNS;
         }
-        void ListBooksAlphabetically(bool Ascending = true)
+          void ListBooksAlphabeticallyy(bool Ascending = true)
         {
+            if(Ascending){
+
+            }
+            else if(!Ascending){
+                
+            }
 
         }
-        void ListBooksChronologicaly(bool Ascending = true)
+        List<Books> ListBooksChronologicaly(bool Ascending = true)
         {
+            List<Books> Chronologically = new List<Books>();
+            if(Ascending){ 
+                for (int i = 0; i < 2025; i++){    
+                   
+                foreach (Books book in BookList){
+                    if (book.publication_year == i){
+                        Chronologically.Add(book);
+                    }
+                }  }
+                return Chronologically;
+
+            }
+            else if(!Ascending){
+                for (int i = 2025; i > 0; i--){    
+                foreach (Books book in BookList){
+                    if (book.publication_year == i){
+                        Chronologically.Add(book);
+                    }
+                } 
+                 }
+                return Chronologically;
+            }
+            else{
+                Console.WriteLine("error");
+                return Chronologically;
+            }
 
         }
+      
         void ListBooksGroupedAuthorLastName()
         {
+            foreach( Books book in BookList){
+                
+              
+                 for (int i = 0; i < NamesList.Length; i++)
+                {
+                }
+
+            }
 
         }
         void ListBooksGroupedAuthorFirstName()
@@ -71,7 +112,7 @@ namespace Exam3
         }
 
     }
-    class books
+    class Books
     {
         public string title { get; set; }
         public int publication_year { get; set; }
