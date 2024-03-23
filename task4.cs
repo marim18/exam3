33c78131a6c0189ef9c6f541d9efb1a162d9a2e3
Maria Møll <marim18@uia.no>
2024-03-23 18:29:38 +0100

    task 4 planned

---
+++ task4.cs
@@ -0,0 +1,32 @@
+namespace Exam3{
+    class task4{
+
+        String ReturnBookByKeyword(string keyword = "the"){
+
+        }
+        String ReturnBookByAuthorContainsLetter(char Keyletter = 't'){
+
+        }
+        int NumberOfBooksAfterYear(int Year = 1992){
+
+        }
+        int NumberOfBooksBeforeYear(int Year = 2004){
+
+        }
+        string ISBNAuthorReturn(){
+
+        }
+        void ListBooksAlphabetically(bool Ascending = true){
+
+        }
+         void ListBooksChronologicaly(bool Ascending = true){
+
+        }
+        void ListBooksGroupedAuthorLastName(){
+
+        }
+         void ListBooksGroupedAuthorFirstName(){
+        }
+        
+    }
+}
