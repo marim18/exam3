d7a7f325d78ad76a5aed6b8338d6801bf190b7f7
Maria Møll <marim18@uia.no>
2024-03-23 18:54:28 +0100

    task 4 ReturnbooksBYKeyword Completed

--- task4.cs
+++ task4.cs
@@ -1,32 +1,64 @@
-namespace Exam3{
-    class task4{
+using System.Text.Json;
+namespace Exam3
+{
+    class task4
+    {
+        public List<books> BookList = new List<books>();
 
-        String ReturnBookByKeyword(string keyword = "the"){
+        public void reader() //redundant refractor at the end ot one function for all if oyu have time
+        {
+
+            string BookString = File.ReadAllText("books.json");
+            books book = JsonSerializer.Deserialize<books>(BookString);
+            BookList.Add(book);
+        }
+        List<books> ReturnBookByKeyword(string keyword = "the")
+        {  List<books> BooksWithKeyword = new List<books>();
+            foreach(books book in BookList){
+                if( book.title.Contains(keyword)){
+                BooksWithKeyword.Add(book);} 
+            }
+            return BooksWithKeyword;
+        }
+
+        String ReturnBookByAuthorContainsLetter(char Keyletter = 't')
+        {
 
         }
-        String ReturnBookByAuthorContainsLetter(char Keyletter = 't'){
+        int NumberOfBooksAfterYear(int Year = 1992)
+        {
 
         }
-        int NumberOfBooksAfterYear(int Year = 1992){
+        int NumberOfBooksBeforeYear(int Year = 2004)
+        {
 
         }
-        int NumberOfBooksBeforeYear(int Year = 2004){
+        string ISBNAuthorReturn()
+        {
 
         }
-        string ISBNAuthorReturn(){
+        void ListBooksAlphabetically(bool Ascending = true)
+        {
 
         }
-        void ListBooksAlphabetically(bool Ascending = true){
+        void ListBooksChronologicaly(bool Ascending = true)
+        {
 
         }
-         void ListBooksChronologicaly(bool Ascending = true){
+        void ListBooksGroupedAuthorLastName()
+        {
 
         }
-        void ListBooksGroupedAuthorLastName(){
+        void ListBooksGroupedAuthorFirstName()
+        {
+        }
 
-        }
-         void ListBooksGroupedAuthorFirstName(){
-        }
-        
+    }
+    class books
+    {
+        public string title { get; set; }
+        public int publication_year { get; set; }
+        public string author { get; set; }
+        public string isbn { get; set; }
     }
 }
