14f603969e918694ed395e23a1842121bb14e79f
Maria Møll <marim18@uia.no>
2024-03-23 17:18:25 +0100

    task 3 managed to read objects from json

--- task3.cs
+++ task3.cs
@@ -1,21 +1,36 @@
 using System.Text.Json;
 
-int SummarizeFunction(JsonDocument JDoc){
+class nodes
+{
+    public int value { get; set; }
+    public string? right { get; set; }
+    public string? left { get; set; }
+    void reader()
+{
+    var NodeString = File.ReadAllText("nodes.json");
+    nodes node = JsonSerializer.Deserialize<nodes>(NodeString);
+}
+
+
+void SummarizeFunction()
+{
     int count = 0; //remember to change names
     int count2 = 0;
     int count3 = 0;
-    foreach value in JDoc{
+    foreach (nodes.value in nodes)
+    {
         count += value;
     }
-    foreach innerarray in JDoc{
+    foreach (innerarray in nodes)
+    { //this needs to change
         count2++;
     }
-    }   foreach node in nodes {
-        count3 ++;
-   }
+    foreach (nodes node)
+    {
+        count3++;
+    }
+    Console.WriteLine($"report: Sum = {count}, depth = {count2}, node = {count3} ");
+
 }
-class nodes{
-    int value = this.value;
-    var right = this.right;
-    var left = this.left;
 }
+
